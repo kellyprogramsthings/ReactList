6eddb9dba97f858e8d705ce83b9d2ac1d3dfd17d
Kelly <kellyprogramsthings@gmail.com>
2019-05-08 15:33:23 -0400

    Updates

---
+++ precache-manifest.3f79d1de4e9ca05a12a549947bb71815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bb737cd3ad28bdfa4ddd05ab6d2eb5",
+    "url": "/kellyprogramsthings/ReactList/index.html"
+  },
+  {
+    "revision": "14e4450d3bd596cf1323",
+    "url": "/kellyprogramsthings/ReactList/static/css/2.067d90b5.chunk.css"
+  },
+  {
+    "revision": "5205f11606a9c7b334dc",
+    "url": "/kellyprogramsthings/ReactList/static/css/main.19d5302d.chunk.css"
+  },
+  {
+    "revision": "14e4450d3bd596cf1323",
+    "url": "/kellyprogramsthings/ReactList/static/js/2.3a6ea356.chunk.js"
+  },
+  {
+    "revision": "5205f11606a9c7b334dc",
+    "url": "/kellyprogramsthings/ReactList/static/js/main.e7b86ddc.chunk.js"
+  },
+  {
+    "revision": "06590c90aa9aa11fb306",
+    "url": "/kellyprogramsthings/ReactList/static/js/runtime~main.5b4bb215.js"
+  }
+]);
